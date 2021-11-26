b3fd253953335c5acaa5373ab54ab583ddbb957e
SinHuJung <tlsgn26@naver.com>
2021-11-26 16:32:28 +0900

    code refactoring

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import React, {useState, useRef, useEffect} from 'react';
+import React, {useState, useRef} from 'react';
 import "./App.css";
 
 function App() {
-  const [number, setNumber] = useState([0]);
+  const [numbers, setNumbers] = useState([0]);
   const [index, setIndex] = useState(0);
   const [input, setInput] = useState('');
-  const [undo, setUndo] = useState(true);
-  const [redo, setRedo] = useState(true);
   const inputRef = useRef(null);
 
   const isInt = (num) => {
@@ -17,38 +15,27 @@ function App() {
     setInput(e.target.value);
   }
 
-  useEffect(() => {
-    index > 0 ? setUndo(false) : setUndo(true);
-    index < number.length - 1 ? setRedo(false) : setRedo(true);
-  },[index])
-
   const buttonClick = (e) => {
     if(e.target.id === "addButton" || e.target.id === "subButton"){
       if(isInt(input)){
         let value = e.target.id === "addButton" ? input : -input;
-        number.splice(index + 1);
-        number.push(number[index] + parseInt(value));
+        setNumbers([...numbers.slice(0, index + 1), Number(numbers[index]) + Number(value)]);
         setIndex(index + 1);
       }
-    } else if(e.target.id === "undoButton"){
-      setIndex(index - 1);
-    } else if (e.target.id === "redoButton"){
-      setIndex(index + 1);
-    }
-
+    } 
     inputRef.current.focus();
     setInput('')
   }
 
   return (
     <div className="container">
-      <div id="valuebox" className="counter">{number[index]}</div>
+      <div id="valuebox" className="counter">{numbers[index]}</div>
       <input ref={inputRef} id="inputbox" className="input" onChange={inputChange} value={input}/>
       <div className="btnGroup">
-        <button id="undoButton" className="btn" onClick={buttonClick} disabled={undo}>Undo</button>
+        <button id="undoButton" className="btn" onClick={() => setIndex(index - 1)} disabled={index > 0 ? false : true}>Undo</button>
         <button id="addButton" className="btn" onClick={buttonClick}>+</button>
         <button id="subButton" className="btn" onClick={buttonClick}>-</button>
-        <button id="redoButton" className="btn" onClick={buttonClick} disabled={redo}>Redo</button>
+        <button id="redoButton" className="btn" onClick={() => setIndex(index + 1)} disabled={index < numbers.length - 1 ? false : true}>Redo</button>
       </div>
     </div>
   );
